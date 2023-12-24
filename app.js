526e0765e822fa52f3cdb5acb5d5e6010f601d62
Rob Caluza <robert.caluza01@gmail.com>
2023-12-24 15:56:36 -0600

    Oh you know just stuff

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import './styles-entry.scss';
+
 "use strict";
 
 // if the page has been rendered, then execute the function
@@ -11,7 +13,9 @@ const getElement = (selector) => {
 
 // convert string into array then loop through each letter and return as a span
 const splitText = (text) => {
-  return [...text].map(letter => `<span class="letter">${letter}</span>`).join("");
+  [...text].map(letter => console.log(letter));
+  return [...text].map(letter =>
+    `<span class="letter" data-text="${letter}">${letter}</span>`).join("");
 }
 
 const fontArray = ["Ft Activica Mix", "Ft Activica Symbols", "Ft Activica"];
@@ -29,9 +33,12 @@ const randomizeLetterFont = (letters) => {
     console.log(`change font ${counter}`);
     const selectLetter = letters[Math.floor(Math.random() * letters.length)];
     const selectFont = fontArray[Math.floor(Math.random() * fontArray.length)];
-    selectLetter.classList.add("glitch-text");
+    const isSymbol = Math.random() > 0.5;
+    const randomClass = isSymbol ? "glitch-text--symbols" : "glitch-text";
+    // randomoize to return glitch-text or glitch-text and --symbols
+    selectLetter.classList.add(randomClass);
     setTimeout(() => {
-      selectLetter.classList.remove("glitch-text");
+      selectLetter.classList.remove("glitch-text", "glitch-text--symbols");
     }, 6000);
   }
 
@@ -46,12 +53,55 @@ const randomizeLetterFont = (letters) => {
   }, 6000);
 }
 
+const nextPhrase = (counter, phrases) => {
+  const nextCounter = counter + 1;
+  if (nextCounter >= phrases.length) {
+    return 0;
+  }
+  return nextCounter;
+}
+
+const textChangeAnimation = (element, phrases) => {
+  let counter = 0;
+  const changeText = () => {
+    element.classList.add("glitch");
+    element.dataset.text = phrases[nextPhrase(counter, phrases)];
+    counter++;
+    if (counter >= phrases.length) {
+      counter = 0;
+    }
+    setTimeout(() => {
+      element.textContent = phrases[counter];
+      setTimeout(() => {
+        element.classList.remove("glitch");
+      }, 500);
+    }, 1000);
+
+
+  }
+  setInterval(changeText, 12000);
+}
+
+const changeText = () => {
+  const header2 = getElement(".heading-2");
+  const span = header2.querySelector("span");
+  span.classList.add("rebel-switcher");
+  const phrases = [
+    "in record time",
+    "efficiently",
+    "with a smile",
+    "with a passion",
+  ];
+  textChangeAnimation(span, phrases);
+}
+
 const heroAnimator = () => {
   const hero = getElement("#animatedHero");
   const header1 = hero.querySelector(".heading-1");
   header1.innerHTML = splitText(header1.innerText);
   const letters = document.querySelectorAll(".letter");
   randomizeLetterFont(letters);
+  changeText();
 }
 
 window.addEventListener("load", () => {
